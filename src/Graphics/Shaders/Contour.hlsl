0f2a172b7702552bbb3852d4585f37f8c3f1a68f
i-saint <saint.skr@gmail.com>
2021-01-21 00:04:13 +0900

    Contour.hlsl

--- src/Graphics/Shaders/Contour.hlsl
+++ src/Graphics/Shaders/Contour.hlsl
@@ -1,6 +1,28 @@
+cbuffer Constants : register(b0)
+{
+    int g_size;
+    int3 g_pad;
+};
 
-[numthreads(1, 1, 1)]
+Texture2D<float> g_image : register(t0);
+RWTexture2D<float> g_result : register(u0);
+
+[numthreads(32, 32, 1)]
 void main(uint2 tid : SV_DispatchThreadID)
 {
-    // todo
+    uint w, h;
+    g_image.GetDimensions(w, h);
+    uint2 ur = uint2(max(tid.x - g_size, 0), max(tid.y - g_size, 0));
+    uint2 bl = uint2(min(tid.x + g_size, w), min(tid.y + g_size, h));
+
+    float cmin, cmax;
+    cmin = cmax = g_image[tid];
+    for (uint i = ur.y; i < bl.y; ++i) {
+        for (uint j = ur.x; j < bl.x; ++j) {
+            float c = g_image[uint2(j, i)];
+            cmin = min(c, cmin);
+            cmax = max(c, cmax);
+        }
+    }
+    g_result[tid] = cmax - cmin;
 }
