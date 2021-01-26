a379e4edf2fcf393c32a4cc14abb610f74a364bf
i-saint <saint.skr@gmail.com>
2021-01-27 08:56:23 +0900

    TemplateMatch

--- src/Graphics/Shaders/TemplateMatch_Binary.hlsl
+++ src/Graphics/Shaders/TemplateMatch_Binary.hlsl
@@ -8,15 +8,57 @@ uint lshift(uint a, uint b, uint s)
     return s == 0 ? a : (a >> s) | (b << (32 - s));
 }
 
+#define CacheCapacity 8192
+groupshared uint s_template[CacheCapacity];
+
+[numthreads(32, 32, 1)]
+void main(uint2 tid : SV_DispatchThreadID, uint gi : SV_GroupIndex)
+{
+    uint w, h;
+    g_template.GetDimensions(w, h);
+
+    const uint pixel_shift = tid.x / 32;
+    const uint bit_shift = tid.x % 32;
+    const uint cache_height = CacheCapacity / w;
+    const uint cache_size = cache_height * w;
+
+    uint r = 0;
+    for (uint i = 0; i < h; ++i) {
+        uint cy = i % cache_height;
+        if (cy == 0) {
+            // preload template
+            GroupMemoryBarrierWithGroupSync();
+            for (uint b = 0; b < 32; ++b) {
+                uint ci = gi * 32 + b;
+                uint ti = w * i + ci;
+                if (ci < cache_size)
+                    s_template[ci] = g_template[uint2(ti % w, ti / w)];
+            }
+            GroupMemoryBarrierWithGroupSync();
+        }
+
+        uint py = tid.y + i;
+        for (uint j = 0; j < w; ++j) {
+            uint px = pixel_shift + j;
+            uint iv = lshift(g_image[uint2(px, py)], g_image[uint2(px + 1, py)], bit_shift);
+            uint tv = s_template[w * cy + j];
+            r += countbits(iv & tv);
+        }
+    }
+    g_result[tid] = r;
+}
+
+#if 0
 [numthreads(32, 32, 1)]
 void main(uint2 tid : SV_DispatchThreadID)
 {
     uint w, h;
     g_template.GetDimensions(w, h);
 
+    const uint pixel_shift = tid.x / 32;
+    const uint bit_shift = tid.x % 32;
+
     uint r = 0;
-    uint pixel_shift = tid.x / 32;
-    uint bit_shift = tid.x % 32;
     for (uint i = 0; i < h; ++i) {
         uint py = tid.y + i;
         for (uint j = 0; j < w; ++j) {
@@ -28,3 +70,4 @@ void main(uint2 tid : SV_DispatchThreadID)
     }
     g_result[tid] = r;
 }
+#endif
